b27a9dde17e4f377607b673e36dd3d25ab92f286
litvinovvo <litvinovvo@gmail.com>
2019-06-12 20:44:29 +0200

    Update and rename big.js to new.js

--- new.js
+++ new.js
@@ -34,25 +34,7 @@
       function(require, module, exports) {
         const bodyScrollLock = require('../../lib/bodyScrollLock.js');
 
-        const disableBodyScrollButton = document.querySelector('.disableBodyScroll');
-        const enableBodyScrollButton = document.querySelector('.enableBodyScroll');
-        const statusElement = document.querySelector('.bodyScrollLockStatus');
-
-        disableBodyScrollButton.onclick = function() {
-          console.info('disableBodyScrollButton');
-          bodyScrollLock.disableBodyScroll(document.querySelector('.scrollTarget'));
-
-          statusElement.innerHTML = ' &mdash; Scroll Locked';
-          statusElement.style.color = 'red';
-        };
-
-        enableBodyScrollButton.onclick = function() {
-          console.info('enableBodyScrollButton');
-          bodyScrollLock.enableBodyScroll(document.querySelector('.scrollTarget'));
-
-          statusElement.innerHTML = ' &mdash; Scroll Unlocked';
-          statusElement.style.color = '';
-        };
+        
       },
       { '../../lib/bodyScrollLock.js': 2 },
     ],
